9e7fa2eb15d097f6d88889a46bd5c458af8be3d0
Yuriy Molchanov <qqmash@gmail.com>
2019-03-24 16:03:29 +0600

    add functions project

---
+++ functions/1.js
@@ -0,0 +1,12 @@
+i=0
+
+function panda()
+{
+	if(i==0)
+	document.getElementById("1").innerHTML="1"
+	else if(i==1)
+	document.getElementById("1").innerHTML="2"
+	else
+	document.getElementById("1").innerHTML="default"
+	i=i+1
+}
